bf3f56a17ca8d5f8ce8fb091db75a55e4b019a32
mvhx <mvhx@g5c06.cin.ufpe.br>
2026-03-10 12:54:17 -0300

    novos exercicios

---
+++ exercicio1/talk_BMO.py
@@ -0,0 +1,26 @@
+name = ""
+age = 0
+princcess = ""
+
+
+print("Olá, eu sou o BMO! Prazer em te conhecer, estranho!")
+
+name = input("Qual é o seu nome?\n")
+if name == "Finn" or name == "Jake":
+    print("Caramba, que coincidência! Você tem o mesmo nome de um amigo meu!")
+
+print("Gostei do seu nome!")
+
+age = input("Quantos anos você tem?\n")
+if age == "12":
+    print("Nossa, você tem a mesma idade do meu amigo Finn")
+
+print("Entendi!")
+
+princcess = input("Ei, qual princesa desse mundo é a mais bonita pra você?\n")
+if princcess == "Princesa de Fogo" or princcess == "Princesa Jujuba":
+    print("Meu amigo Finn vai ficar com ciúmes de você!")
+
+print("Finalmente chegamos!")
+print("Foi um prazer te conhecer, {name}! Boa sorte para encontrar a {princcess} :)")
+
