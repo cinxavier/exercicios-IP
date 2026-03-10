ac36f553be5665d204b6b621906c3af5f0b7f72f
mvhx <mvhx@g5c06.cin.ufpe.br>
2026-03-10 10:01:40 -0300

    update

---
+++ multuply_for_dumbs.py
@@ -0,0 +1,10 @@
+# num1 = int(input("Enter the first number: "))
+# times = int(input("times: "))
+# res = 0
+# for i in range(times):
+#     num1 += (times)
+
+# print("The result is: ", res)
+
+var = int(input("num"))
+print(var)
