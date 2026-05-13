7128c13e9f6d81b1dfe6efe0ba03f00897fb0e8b
mvhx <mvhx@g5c42.cin.ufpe.br>
2026-05-13 13:34:17 -0300

    update: configs Wed May 13 01:34:17 PM -03 2026

--- lista5/q2/idx.py
+++ lista5/q2/idx.py
@@ -8,12 +8,28 @@ def make_pile(n: int, array: list[int] = []):
   return make_pile(n - 1, array)
 
 
-def hanoi(n:int, origin:list[int], aux:list[int], goal:list[int]):
+# a base é:
+# 1. origin -> goal
+# 2. origin -> aux
+# 1. goal -> aux
+# 3. origin -> goal
+# 1. aux -> origin
+# 2. aux -> goal
+# 1. origin -> goal
+
+
+def hanoi(
+  n: int,  # necessario pra saber quando parar
+  origin: list[int],
+  aux: list[int],
+  goal: list[int],
+):
   if n == 0:
     return
+  hanoi(n - 1, origin, aux, goal)
   hanoi(n - 1, origin, goal, aux)
+  hanoi(n - 1, goal, origin, aux)
   goal.append(origin.pop())
-  hanoi(n - 1, goal, aux, origin)
 
 
 origin = make_pile(pile_height)
@@ -22,6 +38,6 @@ goal = []
 
 print(origin, aux, goal)
 
-hanoi(pile_height, origin, goal, aux)
+hanoi(pile_height, origin, aux, goal)
 
 print(origin, aux, goal)
