00be1a85707f431a14f1168763e88f6e43e3ff01
mvhx <mvhx@g2c17.cin.ufpe.br>
2026-05-15 13:39:12 -0300

    update: configs Fri May 15 01:39:12 PM -03 2026

--- lista5/q2/idx.py
+++ lista5/q2/idx.py
@@ -1,57 +1,54 @@
 # desde ja peço desculpas aos monitores que vao corrijir minhas respostas
 # não comentei os códigos por motivos de: sem tempo irmão.
-# estou evocando esse feitiço com meu grimório portátil na longa transição de senário do campo pra a base
-# (tô codando pelo celular no ônibus de volta pra casa com 2 horas de viagem).
+# estou evocando esse feitiço com meu grimório portátil na longa transição de senário da BF pra a base
+# (tô codando pelo celular no ônibus de volta pra casa com 2 horas de viagem. Sem comentários, literalmente).
 
 pile_height = int(input())
 
 
-def make_pile(n: int, array: list[int] = []):
+def make_pile(n: int,array = []):
   if n == 0:
     return array
   array.append(n)
   return make_pile(n - 1, array)
 
 
-def move(origin: list[int], goal: list[int]):
+def move(origin, goal):
   goal.append(origin.pop())
 
 
 def hanoi(
   n: int,  # necessario pra saber quando parar
-  origin: list[int],
-  aux: list[int],
-  goal: list[int],
-  moves: int,
+  origin,
+  aux,
+  goal,
 ):
   if n == 1:
     move(origin, goal)
-    moves += 1
 
   else:
-    hanoi(n - 1, origin, goal, aux, moves)
+    hanoi(n - 1, origin, goal, aux)
     move(origin, goal)
-    moves += 1
 
-    hanoi(n - 1, aux, origin, goal, moves)
+    hanoi(n - 1, aux, origin, goal)
 
 
-def hanoi_moves(pile_height: int):
+def hanoi_moves(pile_height: int) -> int:
   if pile_height == 1:
     return pile_height
+  if pile_height == 2:
+    return pile_height + 1
 
-  return hanoi_moves(pile_height) + hanoi_moves(pile_height)
+  return hanoi_moves(pile_height - 1) * 2 + 1
 
 
-print(hanoi_moves(pile_height))
+origin = make_pile(pile_height)
+aux = []
+goal = []
+moves = hanoi_moves(pile_height)
 
-# origin = make_pile(pile_height)
-# aux = []
-# goal = []
-# moves = 0
+hanoi(pile_height, origin, aux, goal)
 
-# print(origin, aux, goal)
-# hanoi(pile_height, origin, aux, goal, moves)
-# print(moves)
-
-# print(origin, aux, goal)
+print(
+  f"Bela moveu os {pile_height} livros em {moves} movimentos para o Pedestal de Marfim."
+)
